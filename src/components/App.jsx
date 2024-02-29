de90f7a494dc6a061db37ab82e6d10cbf3fec646
Lucas Couchy <lukcouchy@gmail.com>
2024-02-29 14:17:07 +0100

    correction text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,10 +15,10 @@ const App = () => {
 
 
         <Home title="Front-end React Developper">
-          Hey, I'm <span>Lucas Couchy</span> a passionate front-end react developper based in Paris. 
+          Hey, I'm <span>Lucas Couchy</span> a passionate front-end react developper based in Paris.
         </Home>
         <About>
-          Passionate about technology, my initial focus on mechatronics led me into the captivating world of web development. While working on a human-machine interface during my professional degree, I was fascinated by the process of coding applications.<br/>
+          Fascinated by technology, my initial focus on mechatronics led me into the captivating world of web development. While working on a human-machine interface during my professional degree, I was fascinated by the process of coding applications.<br/>
           Building on this experience, I enhanced my skills through online training in web integration. As a self-learner, I quickly mastered the fundamentals, creating dynamic web platforms tailored to clients' needs.<br/>
           Convinced of the importance of continuous learning, my next step will be an apprenticeship. Combining theory and practice in a professional setting is the key to deepening my skills in web development.<br/>
           Every line of code I write aspires to create aesthetic and functional digital experiences. My goal is to harmonize my passion for technology with a pragmatic and creative approach to design innovative web solutions. As an aspiring professional, I am ready to take on new challenges and contribute meaningfully to the world of web development.<br/>
